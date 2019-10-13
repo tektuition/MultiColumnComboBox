b1595084265a3d69a36fe4ea6f63c09675a7e257
shivratankumar <tektuition@gmail.com>
2019-10-13 15:14:01 +0530

    initial commit

---
+++ WebAppMultiColumnComboBox/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppMultiColumnComboBox.Models;
+
+namespace WebAppMultiColumnComboBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeDBEntities objEmployeeDbEntities;
+        public HomeController()
+        {
+            objEmployeeDbEntities = new EmployeeDBEntities();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllEmployees()
+        {
+            return Json(objEmployeeDbEntities.Employees.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebAppMultiColumnComboBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppMultiColumnComboBox.MvcApplication" Language="C#" %>
